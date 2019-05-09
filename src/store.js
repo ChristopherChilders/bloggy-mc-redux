69ac9bf8030774496ec5676335b1cb7e9250fe57
Christopher Childers <christopher.childers13@yahoo.com>
2019-05-09 11:33:04 -0400

    finishes implimenting the localStorage save feature into the app

--- src/store.js
+++ src/store.js
@@ -2,15 +2,35 @@ import { combineReducers, createStore } from 'redux';
 
 import posts from './reducers/posts';
 
+const FREQYENCY = 2000;
+const LS_KEY = 'bloggy-mc-redux';
+
 // Pass and object to combineReducers.
 // this object should be "shaped" like your state.
 const rootReducer = combineReducers({
     posts
 });
 
+// check localStorage for any previously saved app state
+// if it's there, pass as second argument to createStore
+// let initialState = {};
+// const dataFromLocalStorage = JSON.parse(localStorage.getItem(LS_KEY));
+// if (dataFromLocalStorage) {
+//     initialState = dataFromLocalStorage;
+// };
+
+// the same as above, but one line:
+const initialState = JSON.parse(localStorage.getItem(LS_KEY)) || {};
+
 const store = createStore(
     rootReducer,
+    initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+let saveInterval = setInterval(() => {
+    const state = store.getState();
+    localStorage.setItem(LS_KEY, JSON.stringify(state));
+}, FREQYENCY);
 
 export default store;
