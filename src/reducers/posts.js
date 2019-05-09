275fe30d880dc99730b28ee438094d058b249a84
Christopher Childers <christopher.childers13@yahoo.com>
2019-05-09 10:38:27 -0400

    adds action creator delete

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,4 +1,5 @@
 import {ACTION_CREATE_POST} from '../actions';
+import { generateId } from '../utils';
 
 // A reducer is a function that accepts the current state and action,
 // then calculates/returns the next new version of state.
@@ -6,7 +7,18 @@ export default function posts(state={}, action={type: ''}) {
     switch(action.type){
         case ACTION_CREATE_POST:
             // create a post!
-            console.log(`Sounds like you wanna create a post`)
+            const id = generateId();
+            console.log(`the new id is ${id}`);
+            // console.log(`Sounds like you wanna create a post`);
+            const newState = {
+                ...state,
+                // to use a variable as a key in an abject literal,
+                //wrap the vaiable in square brackets.
+                [id]: action.payload
+            };
+            // to get the equivalent result, you can use the familar assignment syntax:
+            // newState[id] = action.payload;
+            return newState;
         break;
         default:
             return state;
